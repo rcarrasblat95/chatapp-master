b57ab5394a352f6e5852f3b87c824c7fbc038b14
rcarrasblat95 <110568017+rcarrasblat95@users.noreply.github.com>
2024-03-21 20:25:15 -0600

    ok

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,6 +2,7 @@ const socket = io()
 
 // Elements
 const $messageForm = document.querySelector('#message-form')
+const $chatBody = document.querySelector('.chat-body');
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location')
@@ -15,51 +16,193 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 // query string
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
+const $messageFormInput2 = document.querySelector('input[name=message]');
+
+// Variable para mantener el temporizador del mensaje de "escribiendo"
+let typingTimer;
+let typingMessageElement; // Elemento del mensaje de "escribiendo"
+
+// Función para mostrar el mensaje de "escribiendo"
+const showTypingMessage = (username) => {
+    // Si ya hay un mensaje de "escribiendo", elimínalo antes de mostrar uno nuevo
+    if (typingMessageElement) {
+        typingMessageElement.remove();
+    }
+
+    // Crear el HTML del mensaje de "escribiendo"
+    const messageHTML = `
+        <div class="message message-in typing-message">
+            <div class="message-inner">
+                <div class="message-body">
+                    <div class="message-content">
+                        <div class="message-text">
+                            <p>${username} escribiendo ...</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="message-footer">
+                    <span class="extra-small text-muted">${moment().format('h:mm A')}</span>
+                </div>
+            </div>
+        </div>`;
+
+    // Agregar el HTML del mensaje de "escribiendo" al chat-body
+    $messages.insertAdjacentHTML('beforeend', messageHTML);
+
+    // Obtener el elemento del mensaje de "escribiendo"
+    typingMessageElement = document.querySelector('.typing-message');
+
+    // Hacer scroll automáticamente
+    autoscroll();
+
+    // Reiniciar el temporizador
+    clearTimeout(typingTimer);
+
+    // Configurar un temporizador para borrar el mensaje después de un cierto tiempo
+    typingTimer = setTimeout(() => {
+        // Borrar el mensaje de "escribiendo"
+        if (typingMessageElement) {
+            typingMessageElement.remove();
+            typingMessageElement = null; // Restablecer la referencia al elemento
+        }
+    }, 300); // Cambiar el tiempo según tus necesidades
+};
+
+socket.on('typing', (username) => {
+    // Muestra el mensaje de "escribiendo"
+    showTypingMessage(username);
+});
+
+
+
+
 const autoscroll = () => {
-    const $newMessage = $messages.lastElementChild
-    const newMessageStyles = getComputedStyle($newMessage)
-    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
-    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
-    const visibleHeight = $messages.offsetHeight
-    const containerHeight = $messages.scrollHeight
+    const $newMessage = $messages.lastElementChild;
+    const newMessageStyles = getComputedStyle($newMessage);
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+    const visibleHeight = $chatBody.offsetHeight; // Usamos $chatBody en lugar de $messages
+    const containerHeight = $chatBody.scrollHeight; // Usamos $chatBody en lugar de $messages.scrollHeight
 
     // How far have I scrolled?
-    const scrollOffset = $messages.scrollTop + visibleHeight
+    const scrollOffset = $chatBody.scrollTop + visibleHeight;
 
     if (containerHeight - newMessageHeight <= scrollOffset) {
-        $messages.scrollTop = $messages.scrollHeight
+        $chatBody.scrollTop = $chatBody.scrollHeight;
     }
-}
+};
 
 socket.on('message', (message) => {
-    console.log(message)
-    const html = Mustache.render(messageTemplate, {
-        username: message.username,
-        message: message.text,
-        createdAt: moment(message.createdAt).format('h:mm a')
-    })
-    $messages.insertAdjacentHTML('beforeend', html)
-    autoscroll()
-})
+    // Convertir el nombre de usuario actual a minúsculas
+    const currentUser = username.toLowerCase();
+
+    // Convertir el nombre de usuario del mensaje a minúsculas
+    const messageUser = message.username.toLowerCase();
+
+    // Determinar si el mensaje es del usuario actual
+    const isCurrentUser = messageUser === currentUser;
+
+    // Agregar una clase CSS basada en si el mensaje es del usuario actual
+    const messageClass = isCurrentUser ? 'message message-out' : 'message';
+
+    // Obtener la hora formateada
+    const formattedTime = moment(message.createdAt).format('h:mm A');
+
+    // Crear el HTML del mensaje
+    const html = `
+        <div class="${messageClass}">
+            <div class="message-inner">
+                <div class="message-body">
+                    <div class="message-content">
+                        <div class="message-text">
+                        <p>${message.username}</p>
+                            <p>${message.text}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="message-footer">
+                    <span class="extra-small text-muted">${formattedTime}</span>
+                </div>
+            </div>
+        </div>`;
+
+    // Insertar el HTML del mensaje en el DOM
+    $messages.insertAdjacentHTML('beforeend', html);
+
+    // Hacer scroll automáticamente
+    autoscroll();
+});
+
+// Insertar el nombre de la sala en el HTML
+document.getElementById('roomName').innerText = room;
+
 
 socket.on('locationMessage', (message) => {
-    console.log(message)
-    const html = Mustache.render(locationMessageTemplate, {
-        username: message.username,
-        url: message.url,
-        createdAt: moment(message.createdAt).format('h:mm a')
-    })
-    $messages.insertAdjacentHTML('beforeend', html)
-    autoscroll()
-})
+    // Determinar si el mensaje es del usuario actual
+    const isCurrentUser = message.username === username;
+
+    // Agregar una clase CSS basada en si el mensaje es del usuario actual
+    const messageClass = isCurrentUser ? 'message message-out' : 'message';
+
+    // Obtener la hora formateada
+    const formattedTime = moment(message.createdAt).format('h:mm A');
+
+    // Crear el HTML del mensaje
+    const html = `
+        <div class="${messageClass}">
+            <div class="message-inner">
+                <div class="message-body">
+                    <div class="message-content">
+                        <div class="message-text">
+                        <p>${user.username}</p>
+                            <p><a href="${message.url}" target="_blank">Location shared</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="message-footer">
+                    <span class="extra-small text-muted">${formattedTime}</span>
+                </div>
+            </div>
+        </div>`;
+
+    // Insertar el HTML del mensaje en el DOM
+    $messages.insertAdjacentHTML('beforeend', html);
+
+    // Hacer scroll automáticamente
+    autoscroll();
+});
+
 
 socket.on('roomData', ({ room, users }) => {
-    const html = Mustache.render(sidebarTemplate, {
-        room,
-        users
-    })
-    document.querySelector('#sidebar').innerHTML = html
-})
+    const $sidebar = document.querySelector('#sidebar');
+
+    // Limpiar contenido anterior del sidebar
+    $sidebar.innerHTML = '';
+
+    // Generar HTML para cada usuario y agregarlo al sidebar
+    users.forEach(user => {
+        const html = `
+            <div  class="card border-0 text-reset">
+                <div class="card-body">
+                    <div class="row gx-5">
+                        <div class="col">
+                            <div class="d-flex align-items-center mb-3">
+                                <h5 class="me-auto mb-0">${user.username}</h5>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <div class="line-clamp me-auto">
+                                    ${user.typing ? 'is typing<span class="typing-dots"><span>.</span><span>.</span><span>.</span></span>' : ''}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+
+        // Insertar HTML generado en el sidebar
+        $sidebar.insertAdjacentHTML('beforeend', html);
+    });
+});
 
 $messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -94,7 +237,7 @@ $sendLocationButton.addEventListener('click', () => {
             longitude: position.coords.longitude
         }, () => {
             $sendLocationButton.removeAttribute('disabled')
-            console.log('Location shared!')  
+            console.log('Location shared!')
         })
     })
 })
--- src/index.js
+++ src/index.js
@@ -19,51 +19,77 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.on('join', (options, callback) => {
-        const { error, user } = addUser({ id: socket.id, ...options })
+        try {
+            const { error, user } = addUser({ id: socket.id, ...options })
 
-        if (error) {
-            return callback(error)
-        }
+            if (error) {
+                throw new Error(error)
+            }
 
-        socket.join(user.room)
+            socket.join(user.room)
 
-        socket.emit('message', generateMessage('Admin', 'Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
-        io.to(user.room).emit('roomData', {
-            room: user.room,
-            users: getUsersInRoom(user.room)
-        })
-
-        callback()
-    })
-
-    socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id)
-        const filter = new Filter()
-
-        if (filter.isProfane(message)) {
-            return callback('Profanity is not allowed!')
-        }
-
-        io.to(user.room).emit('message', generateMessage(user.username, message))
-        callback()
-    })
-
-    socket.on('sendLocation', (coords, callback) => {
-        const user = getUser(socket.id)
-        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
-        callback()
-    })
-
-    socket.on('disconnect', () => {
-        const user = removeUser(socket.id)
-
-        if (user) {
-            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            socket.emit('message', generateMessage('Admin', 'Welcome!'))
+            socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
             io.to(user.room).emit('roomData', {
                 room: user.room,
                 users: getUsersInRoom(user.room)
             })
+
+            callback()
+        } catch (error) {
+            callback(error.message)
+        }
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        try {
+            const user = getUser(socket.id)
+            const filter = new Filter()
+
+            if (filter.isProfane(message)) {
+                throw new Error('Profanity is not allowed!')
+            }
+
+            io.to(user.room).emit('message', generateMessage(user.username, message))
+            callback()
+        } catch (error) {
+            callback(error.message)
+        }
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        try {
+            const user = getUser(socket.id)
+            io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+            callback()
+        } catch (error) {
+            callback(error.message)
+        }
+    })
+
+    // Agregar un evento 'typing' al servidor
+    socket.on('typing', () => {
+        try {
+            const user = getUser(socket.id)
+            socket.broadcast.to(user.room).emit('typing', user.username)
+        } catch (error) {
+            console.error(error)
+        }
+    })
+
+    socket.on('disconnect', () => {
+        try {
+            const user = removeUser(socket.id)
+
+            if (user) {
+                io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+                io.to(user.room).emit('roomData', {
+                    room: user.room,
+                    users: getUsersInRoom(user.room)
+                })
+            }
+        } catch (error) {
+            console.error(error)
         }
     })
 })
