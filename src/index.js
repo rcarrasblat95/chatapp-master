b9b369fb762df9706c84304ef59e2ae0dddde413
rcarrasblat95 <110568017+rcarrasblat95@users.noreply.github.com>
2024-03-22 11:18:06 -0600

    ok

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
-const socketio = require('socket.io')
+// const socketio = require('socket.io')
 const Filter = require('bad-words')
 const { generateMessage, generateLocationMessage } = require('./utils/messages')
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
+const io = require('socket.io')(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
